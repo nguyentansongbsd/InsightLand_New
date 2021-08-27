5ef3a6ca33ede86bfed68e4613256a016d6e9880
phu <phamxuanphu98@gmail.com>
2021-08-27 15:17:54 +0700

    commit task 56

---
+++ ConasiCRM/Portable/ViewModels/ActivityListViewModel.cs
@@ -0,0 +1,59 @@
+﻿using ConasiCRM.Portable.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ConasiCRM.Portable.ViewModels
+{
+    public class ActivityListViewModel : ListViewBaseViewModel2<HoatDongListModel>
+    {
+        public string Keyword { get; set; }
+
+        public ActivityListViewModel()
+        {
+            PreLoadData = new Command(() =>
+            {
+                EntityName = "activitypointers";
+                FetchXml = $@"<fetch version='1.0' count='15' page='{Page}' output-format='xml-platform' mapping='logical' distinct='true'>
+                                  <entity name='activitypointer'>
+                                    <attribute name='activitytypecode' />
+                                    <attribute name='subject' />
+                                    <attribute name='statecode' />
+                                    <attribute name='prioritycode' />
+                                    <attribute name='modifiedon' />
+                                    <attribute name='activityid' />
+                                    <attribute name='instancetypecode' />
+                                    <attribute name='community' />
+                                    <attribute name='regardingobjectid' />
+                                    <attribute name='scheduledstart' />
+                                    <attribute name='scheduledend' />
+                                    <attribute name='createdon' />
+                                    <attribute name='actualdurationminutes' />
+                                    <attribute name='description' />
+                                    <order attribute='modifiedon' descending='false' />
+                                    <filter type='and'>
+                                      <condition attribute='activitytypecode' operator='in'>
+                                        <value>4210</value>
+                                        <value>4201</value>
+                                        <value>4212</value>
+                                      </condition>
+                                    </filter>
+                                    <filter type='and'>
+                                        <condition attribute='subject' operator='like' value='%{Keyword}%' />
+                                     </filter>
+                                    <link-entity name='account' from='accountid' to='regardingobjectid' link-type='outer' alias='ae'>
+                                        <attribute name='bsd_name' alias='accounts_bsd_name'/>
+                                    </link-entity>
+                                    <link-entity name='contact' from='contactid' to='regardingobjectid' link-type='outer' alias='af'>
+                                        <attribute name='fullname' alias='contact_bsd_fullname'/>
+                                    </link-entity>
+                                    <link-entity name='lead' from='leadid' to='regardingobjectid' link-type='outer' alias='ag'>
+                                        <attribute name='fullname' alias='lead_fullname'/>
+                                    </link-entity>
+                                  </entity>
+                                </fetch>";
+            });
+        }
+    }
+}
---
+++ ConasiCRM/Portable/Views/ActivityList.xaml.cs
@@ -0,0 +1,117 @@
+﻿using ConasiCRM.Portable.Helper;
+using ConasiCRM.Portable.Models;
+using ConasiCRM.Portable.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConasiCRM.Portable.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActivityList : ContentPage
+    {
+        public Action<bool> OnCompleted;
+        public ActivityListViewModel viewModel;
+        public ActivityList()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new ActivityListViewModel();
+            Init();
+        }
+        public async void Init()
+        {
+            await viewModel.LoadData();
+            if (viewModel.Data.Count > 0)
+            {
+                OnCompleted?.Invoke(true);
+            }
+            else
+            {
+                OnCompleted?.Invoke(false);
+            }
+        }
+
+        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            LoadingHelper.Show();
+            //HoatDongListModel val = e.Item as HoatDongListModel;
+            //if (val.activitytypecode == "task")
+            //{
+            //    TaskForm newPage = new TaskForm(val.activityid);
+            //    newPage.CheckTaskForm = async (CheckEventData) =>
+            //    {
+            //        if (CheckEventData == true)
+            //        {
+            //            await Navigation.PushAsync(newPage);
+            //        }
+            //        LoadingHelper.Hide();
+            //    };
+            //}
+            //else if (val.activitytypecode == "phonecall")
+            //{
+            //    PhoneCallForm newPage = new PhoneCallForm(val.activityid);
+            //    newPage.CheckPhoneCell = async (CheckEventData) =>
+            //    {
+            //        if (CheckEventData == true)
+            //        {
+            //            await Navigation.PushAsync(newPage);
+            //        }
+            //        LoadingHelper.Hide();
+            //    };
+            //}
+            //else if (val.activitytypecode == "appointment")
+            //{
+            //    MeetingForm newPage = new MeetingForm(val.activityid);
+            //    newPage.CheckMeeting = async (CheckEventData) =>
+            //    {
+            //        if (CheckEventData == true)
+            //        {
+            //            await Navigation.PushAsync(newPage);
+            //        }
+            //        LoadingHelper.Hide();
+            //    };
+            //}
+            LoadingHelper.Hide();
+        }
+
+        private async void SearchBar_SearchButtonPressed(System.Object sender, System.EventArgs e)
+        {
+            LoadingHelper.Show();
+            await viewModel.LoadOnRefreshCommandAsync();
+            LoadingHelper.Hide();
+        }
+
+        private void SearchBar_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(viewModel.Keyword))
+            {
+                SearchBar_SearchButtonPressed(null, EventArgs.Empty);
+            }
+        }
+
+        private async Task NewActivity_ClickedAsync(object sender, EventArgs e)
+        {
+            LoadingHelper.Show();
+            string[] options = new string[] { "Tạo Cuộc Họp", "Tạo Cuộc Gọi", "Tạo Công Việc" };
+            string asw = await DisplayActionSheet("Tuỳ chọn", "Hủy", null, options);
+            if (asw == "Tạo Cuộc Họp")
+            {
+                await Navigation.PushAsync(new LeadForm());
+            }
+            else if (asw == "Tạo Cuộc Gọi")
+            {
+                await Navigation.PushAsync(new ContactForm());
+            }
+            else if (asw == "Tạo Công Việc")
+            {
+                await Navigation.PushAsync(new AccountForm());
+            }
+            LoadingHelper.Hide();
+        }
+    }
+}
