eb6aae4d6c0c649479ed825be359dce58f361d7e
phu <phamxuanphu98@gmail.com>
2021-09-06 08:17:01 +0700

    phone call, thieu employee

--- ConasiCRM/Portable/ViewModels/PhoneCallViewModel.cs
+++ ConasiCRM/Portable/ViewModels/PhoneCallViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,14 +21,15 @@ namespace ConasiCRM.Portable.ViewModels
         public List<List<OptionSet>> AllsLookUp { get; set; }
         public List<string> Tabs { get; set; }
 
-        private LookUp _customer;
-        public LookUp Customer { get => _customer; set { _customer = value; OnPropertyChanged(nameof(Customer)); } }
+        private OptionSet _customer;
+        public OptionSet Customer { get => _customer; set { _customer = value; OnPropertyChanged(nameof(Customer)); } }
 
         private OptionSet _callFrom;
         public OptionSet CallFrom { get => _callFrom; set { _callFrom = value; OnPropertyChanged(nameof(CallFrom)); } }
 
         public List<string> _callTo;
         public List<string> CallTo { get => _callTo; set { _callTo = value; OnPropertyChanged(nameof(CallTo)); } }
+        public List<OptionSet> CallToOptionSet { get; set; }
 
         string CodeAccount = "3";
 
@@ -37,14 +39,130 @@ namespace ConasiCRM.Portable.ViewModels
 
         public PhoneCallViewModel()
         {
+            PhoneCellModel = new PhoneCellModel();
             ContactsLookUp = new List<OptionSet>();
             LeadsLookUp = new List<OptionSet>();
             AccountsLookUp = new List<OptionSet>();
             AllsLookUp = new List<List<OptionSet>>();
+            CallToOptionSet = new List<OptionSet>();
             Tabs = new List<string>();
             Tabs.Add("KH Tiềm Năng");
             Tabs.Add("KH Cá Nhân");
             Tabs.Add("KH Doanh Nghiệp");
+        }
+
+        public async Task<Boolean> DeletLookup(string fieldName, Guid id)
+        {
+            var result = await CrmHelper.SetNullLookupField("phonecalls", id, fieldName);
+            return result.IsSuccess;
+        }
+        private async Task<object> getContent(string value)
+        {
+            IDictionary<string, object> data = new Dictionary<string, object>();
+            data["activityid"] = PhoneCellModel.activityid.ToString();
+            data["subject"] = PhoneCellModel.subject;
+            data["description"] = PhoneCellModel.description;
+            data["scheduledstart"] = PhoneCellModel.scheduledstart.Value.ToUniversalTime();
+            data["scheduledend"] = PhoneCellModel.scheduledend.Value.ToUniversalTime();
+            // data["actualdurationminutes"] = dataPhone.actualdurationminutes;
+            if (value == "update")
+            {
+                data["statecode"] = PhoneCellModel.statecode;
+                data["statuscode"] = PhoneCellModel.statuscode;
+            }
+            data["phonenumber"] = PhoneCellModel.phonenumber;
+
+            if (Customer != null)
+            {
+                if (Customer.Title == CodeLead)
+                {
+                    data["regardingobjectid_lead_phonecall@odata.bind"] = "/leads(" + Customer.Val + ")";
+                }
+                else if (Customer.Title == CodeContac)
+                {
+                    data["regardingobjectid_contact_phonecall@odata.bind"] = "/contacts(" + Customer.Val + ")";
+                }
+                else if (Customer.Title == CodeAccount)
+                {
+                    data["regardingobjectid_account_phonecall@odata.bind"] = "/accounts(" + Customer.Val + ")";
+                }
+            }
+            else
+            {
+                await DeletLookup("regardingobjectid_contact_phonecall", PhoneCellModel.activityid);
+                await DeletLookup("regardingobjectid_account_phonecall", PhoneCellModel.activityid);
+                await DeletLookup("regardingobjectid_lead_phonecall", PhoneCellModel.activityid);
+            }
+
+            List<object> dataFromTo = new List<object>();
+
+            IDictionary<string, object> item_from = new Dictionary<string, object>();
+            if (CallFrom.Title == CodeLead)
+            {
+                item_from["partyid_lead@odata.bind"] = "/leads(" + CallFrom.Val + ")";
+                item_from["participationtypemask"] = 1;
+            }
+            else if (CallFrom.Title == CodeContac)
+            {
+                item_from["partyid_contact@odata.bind"] = "/contacts(" + CallFrom.Val + ")";
+                item_from["participationtypemask"] = 1;
+            }
+            else if (CallFrom.Title == CodeAccount)
+            {
+                item_from["partyid_account@odata.bind"] = "/accounts(" + CallFrom.Val + ")";
+                item_from["participationtypemask"] = 1;
+            }
+
+            dataFromTo.Add(item_from);
+
+            foreach (var item in CallToOptionSet)
+            {
+                IDictionary<string, object> item_to = new Dictionary<string, object>();
+                if (item.Title == CodeLead)
+                {
+                    item_to["partyid_lead@odata.bind"] = "/leads(" + item.Val + ")";
+                    item_to["participationtypemask"] = 2;
+                }
+                else if (item.Title == CodeContac)
+                {
+                    item_to["partyid_contact@odata.bind"] = "/contacts(" + item.Val + ")";
+                    item_to["participationtypemask"] = 2;
+                }
+                else if (item.Title == CodeAccount)
+                {
+                    item_to["partyid_account@odata.bind"] = "/accounts(" + item.Val + ")";
+                    item_to["participationtypemask"] = 2;
+                }
+                dataFromTo.Add(item_to);
+            }
+
+            data["phonecall_activity_parties"] = dataFromTo;
+
+            //if (UserLogged.Id != Guid.Empty)
+            //{
+            //    data["bsd_employee@odata.bind"] = "/bsd_employees(" + UserLogged.Id + ")";
+            //}
+            if (UserLogged.ManagerId != Guid.Empty)
+            {
+                data["ownerid@odata.bind"] = "/systemusers(" + UserLogged.ManagerId + ")";
+            }
+            return data;
+        }
+
+        public async Task<bool> createPhoneCall()
+        {
+            PhoneCellModel.activityid = Guid.NewGuid();
+            string path = "/phonecalls";
+            var content = await this.getContent("create");
+            CrmApiResponse result = await CrmHelper.PostData(path, content);
+            if (result.IsSuccess)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public async Task LoadLeadsLookUp()
@@ -129,6 +247,25 @@ namespace ConasiCRM.Portable.ViewModels
                 AllsLookUp.Add(LeadsLookUp);
                 AllsLookUp.Add(ContactsLookUp);
                 AllsLookUp.Add(AccountsLookUp);
+            }
+        }
+
+        public void ConvertStringToOptionSet()
+        {
+            if (CallTo != null && CallTo.Count > 0)
+            {
+                foreach (var id in CallTo)
+                {
+                    var item = new OptionSet();
+                    item.Val = id;
+                    if (LeadsLookUp.SingleOrDefault(x => x.Val == id) != null)
+                        item.Title = CodeLead;
+                    else if (ContactsLookUp.SingleOrDefault(x => x.Val == id) != null)
+                        item.Title = CodeContac;
+                    else if (AccountsLookUp.SingleOrDefault(x => x.Val == id) != null)
+                        item.Title = CodeAccount;
+                    CallToOptionSet.Add(item);
+                }
             }
         }
     }
--- ConasiCRM/Portable/Views/PhoneCallForm.xaml.cs
+++ ConasiCRM/Portable/Views/PhoneCallForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ConasiCRM.Portable.Controls;
 using ConasiCRM.Portable.Helper;
+using ConasiCRM.Portable.Helpers;
 using ConasiCRM.Portable.Models;
 using ConasiCRM.Portable.ViewModels;
 using System;
@@ -66,27 +67,168 @@ namespace ConasiCRM.Portable.Views
 
         private void CreatePhoneCall_Clicked(object sender, EventArgs e)
         {
+            SaveData();
+        }
 
+        private async void SaveData()
+        {
+            if (string.IsNullOrWhiteSpace(viewModel.PhoneCellModel.subject))
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng nhập chủ đề cuộc gọi");
+                return;
+            }
+            if (viewModel.CallFrom == null)
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn người thực hiện cuộc gọi");
+                return;
+            }
+            if (viewModel.CallTo == null)
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn người nhận cuộc gọi");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(viewModel.PhoneCellModel.phonenumber))
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng nhập số điện thoại");
+                return;
+            }
+            if (viewModel.PhoneCellModel.scheduledstart == null || viewModel.PhoneCellModel.scheduledend == null)
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc và thời gian bắt đầu");
+                    return;
+            }
+            if (viewModel.PhoneCellModel.scheduledstart != null && viewModel.PhoneCellModel.scheduledend != null)
+            {
+                if (this.compareDateTime(viewModel.PhoneCellModel.scheduledstart, viewModel.PhoneCellModel.scheduledend) != -1)
+                {
+                    ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc lớn hơn thời gian bắt đầu");
+                    return;
+                }
+            }
+            viewModel.ConvertStringToOptionSet();
+            if (await viewModel.createPhoneCall())
+            {
+                ToastMessageHelper.ShortMessage("Đã thêm cuộc gọi");
+            }
+            else
+            {
+                ToastMessageHelper.ShortMessage("Thêm cuộc gọi thất bại");
+            }
         }
 
         private void DateEnd_DateSelected(object sender, DateChangedEventArgs e)
         {
+            DateTime timeNew = e.NewDate;
+            TimeSpan time = viewModel.PhoneCellModel.timeEnd;
+            DateTime _scheduledend = new DateTime(timeNew.Year, timeNew.Month, timeNew.Day, time.Hours, time.Minutes, time.Seconds);
 
+            if (viewModel.PhoneCellModel.scheduledstart != null )
+            {
+                if (this.compareDateTime(viewModel.PhoneCellModel.scheduledstart, _scheduledend) == -1)
+                {
+                    viewModel.PhoneCellModel.scheduledend = _scheduledend;
+                }
+                else
+                {
+                    ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc lớn hơn thời gian bắt đầu");
+                    viewModel.PhoneCellModel.scheduledend = viewModel.PhoneCellModel.scheduledstart;               
+                }
+            }
+            else
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian bắt đầu");
+            }
         }
 
         private void DateStart_DateSelected(object sender, DateChangedEventArgs e)
         {
-
+            DateTime timeNew = e.NewDate;
+            TimeSpan time = viewModel.PhoneCellModel.timeStart;
+            var scheduledstart = new DateTime(timeNew.Year, timeNew.Month, timeNew.Day, time.Hours, time.Minutes, time.Seconds);
+            if (viewModel.PhoneCellModel.scheduledend != null)
+            {
+                if (this.compareDateTime(scheduledstart, viewModel.PhoneCellModel.scheduledend) == -1)
+                {
+                    viewModel.PhoneCellModel.scheduledstart = scheduledstart;
+                }
+                else
+                {
+                    ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc lớn hơn thời gian bắt đầu");
+                    viewModel.PhoneCellModel.scheduledstart = viewModel.PhoneCellModel.scheduledend;
+                }
+            }
+            else
+            {
+                viewModel.PhoneCellModel.scheduledstart = scheduledstart;
+            }
         }
 
-        private void TimeStart_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private int compareDateTime(DateTime? date, DateTime? date1)
         {
+            if (date != null && date1 != null )
+            {
+                int result = DateTime.Compare(date.Value, date1.Value);
+                if (result < 0)
+                    return -1;
+                else if (result == 0)
+                    return 0;
+                else
+                    return 1;
+            }
+            if (date == null && date1 != null)
+                return -1;
+            if (date1 == null && date != null)
+                return 1;
+            return 0;
+        }     
 
+        private void TimeEnd_Unfocused(object sender, FocusEventArgs e)
+        {
+            if (viewModel.PhoneCellModel.scheduledend != null && viewModel.PhoneCellModel.scheduledstart != null)
+            {
+                var timeNew = timePickerEnd.Time;
+                DateTime time = viewModel.PhoneCellModel.scheduledend.Value;
+                DateTime _scheduledend = new DateTime(time.Year, time.Month, time.Day, timeNew.Hours, timeNew.Minutes, timeNew.Seconds);
+                if (this.compareDateTime(viewModel.PhoneCellModel.scheduledstart.Value, _scheduledend) == -1)
+                {
+                    viewModel.PhoneCellModel.timeEnd = timeNew;
+                    viewModel.PhoneCellModel.scheduledend = _scheduledend;
+                }
+                else
+                {
+                    ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc lớn hơn thời gian bắt đầu");
+                    viewModel.PhoneCellModel.timeEnd = viewModel.PhoneCellModel.timeStart;
+                }
+            }
+            else
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn ngày bắt đầu và ngày kết thúc");
+            }
         }
 
-        private void TimeEnd_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private void TimeStart_Unfocused(object sender, FocusEventArgs e)
         {
+            if (viewModel.PhoneCellModel.scheduledend != null && viewModel.PhoneCellModel.scheduledstart != null)
+            {
+                var timeNew = timePickerStart.Time;
+                DateTime time = viewModel.PhoneCellModel.scheduledstart.Value;
+                DateTime scheduledstart = new DateTime(time.Year, time.Month, time.Day, timeNew.Hours, timeNew.Minutes, timeNew.Seconds);
 
+                if (this.compareDateTime(scheduledstart, viewModel.PhoneCellModel.scheduledend.Value) == -1)
+                {
+                    viewModel.PhoneCellModel.timeStart = timeNew;
+                    viewModel.PhoneCellModel.scheduledstart = scheduledstart;
+                }
+                else
+                {
+                    ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian kết thúc lớn hơn thời gian bắt đầu");
+                    viewModel.PhoneCellModel.timeStart = viewModel.PhoneCellModel.timeEnd;
+                }
+            }
+            else
+            {
+                ToastMessageHelper.ShortMessage("Vui lòng chọn thời gian bắt đầu và thời gian kết thúc");
+            }
         }
     }
 }
