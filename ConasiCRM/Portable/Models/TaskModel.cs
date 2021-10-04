beb9f6f5b6b78cd74c3d26e8462dbcfa5d247f43
nguyentansongbsd <songnt@bsdinsight.com>
2021-10-04 18:56:54 +0700

    song done task 101: danh sach Cong viec

---
+++ ConasiCRM/Portable/Models/TaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConasiCRM.Portable.Models
+{
+    public class TaskModel
+    {
+        public Guid activityid { get; set; }
+        public string subject { get; set; }
+
+        public string customer { get; set; }
+        public Guid contact_id { get; set; }
+        public string contact_name { get; set; }
+
+        public Guid account_id { get; set; }
+        public string account_name { get; set; }
+
+        public DateTime scheduledstart { get; set; }
+        public DateTime scheduledend { get; set; }
+        public DateTime createdon { get; set; }
+    }
+}
