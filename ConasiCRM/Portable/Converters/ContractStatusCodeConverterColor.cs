f6fc4721716c6cecaa61e7e54e081bd47416bc79
phu <phamxuanphu98@gmail.com>
2021-10-11 17:55:57 +0700

    task 95 96

---
+++ ConasiCRM/Portable/Converters/ContractStatusCodeConverterColor.cs
@@ -0,0 +1,29 @@
+﻿using ConasiCRM.Portable.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ConasiCRM.Portable.Converters
+{
+    public class ContractStatusCodeConverterColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return ContractStatusCodeData.GetContractStatusCodeById(value.ToString()).Background;
+            }
+            else
+            {
+                return "#bfbfbf";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConasiCRM/Portable/Models/ContractModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConasiCRM.Portable.Models
+{
+    public class ContractModel
+    {
+        public Guid salesorderid { get; set; } // id hợp đồng
+        public string ordernumber { get; set; } // số hợp đồng
+        public Guid project_id { get; set; }
+        public string project_name { get; set; }
+        public Guid unit_id { get; set; }
+        public string unit_name { get; set; }
+        public decimal totalamount { get; set; } // tổng tiền
+        public int statuscode { get; set; }
+        public string statuscode_format { get => ContractStatusCodeData.GetContractStatusCodeById(statuscode.ToString()).Name; }
+        public Guid customerid { get; set; } // id khách hàng
+        public string customer_name // tên khách hàng 
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(account_name))
+                {
+                    return account_name;
+                }
+                else
+                {
+                    return contact_name ?? "";
+                }
+            }
+        }
+        public string contact_name { get; set; } // tên khách hàng cá nhân
+        public string account_name { get; set; } // tên khách hàng doanh nghiệp
+    }
+}
